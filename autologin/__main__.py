b9e0ae0ec2e1a8889114b5574d6b42b2592f2df6
cafehaine <kilian.guillaume@gmail.com>
2021-09-29 10:17:23 +0200

    Start implementing config loading

--- autologin/__main__.py
+++ autologin/__main__.py
@@ -1,6 +1,10 @@
 from abc import ABC, abstractmethod
+from argparse import ArgumentParser
+from configparser import ConfigParser
 from datetime import timedelta
+import os
 import random
+import sys
 from time import sleep
 from typing import Dict, Type
 
@@ -15,9 +19,15 @@ PORTAL_DETECT_URLS: Dict[str, str] = {
 
 CHECK_PERIOD = timedelta(seconds=60)
 
+CONFIG_EXPECTED_PATHS = [
+    "~/.config/autologin/config.ini",
+    "~/.autologin.ini",
+    "/etc/autologin.ini",
+]
+
 
 class PortalHandler(ABC):
-    def __init__(self, config) -> None:
+    def __init__(self, config: ConfigParser) -> None:
         self._config = config
 
     @abstractmethod
@@ -36,14 +46,14 @@ def get_portal_handler(url: str, portal: str) -> Type[PortalHandler]:
     return ULCOPortalHandler
 
 
-def login(config, url: str, portal: str) -> None:
+def login(config: ConfigParser, url: str, portal: str) -> None:
     """Select a portal handler, and then call the login method."""
     portal_handler = get_portal_handler(url, portal)
     print(f"Detected portal of type {portal_handler.__class__.__name__}.")
     portal_handler(config).login(url, portal)
 
 
-def check_online(config) -> None:
+def check_online(config: ConfigParser) -> None:
     """Check if the computer is online, and login if a portal is detected."""
     url, expected_content = random.choice(list(PORTAL_DETECT_URLS.items()))
     try:
@@ -59,14 +69,52 @@ def check_online(config) -> None:
     login(config, result.url, result.text)
 
 
+def load_config(path: str) -> ConfigParser:
+    """Load a config from a given path."""
+    config = ConfigParser()
+    config.read(path)
+    return config
+
+
 def main():
     """Check periodically if the computer is online."""
+    parser = ArgumentParser()
+    parser.add_argument("--configuration-path", "-c", nargs=1, type=str, default=None)
+
+    args = parser.parse_args()
+
+    config: Optional[ConfigParser] = None
+
+    if args.configuration_path is not None:
+        path = args.configuration_path[0]
+        if not os.path.exists(path):
+            print(f"Config file {path!r} not found.")
+            sys.exit(1)
+        config = load_config(path)
+    else:
+        for path in CONFIG_EXPECTED_PATHS:
+            if os.path.exists(path):
+                config = load_config(path)
+                break
+
+    if config is None:
+        print(
+            "Could not find any configuration file, please create one at one of the expected paths, or pass it as an argument."
+        )
+        print("Expected paths are the following:")
+        for path in CONFIG_EXPECTED_PATHS:
+            print("-", path)
+        sys.exit(1)
+
     print("autologin started")
-    # TODO really load config
-    config = {"ulco": ("login", "password")}
+
+    sleep_duration = config.getint(
+        "general", "update_period", fallback=CHECK_PERIOD.total_seconds()
+    )
 
     while True:
-        sleep(CHECK_PERIOD.total_seconds())
+        print(f"Waiting for {sleep_duration}s before checking.")
+        sleep(sleep_duration)
         check_online(config)
 
 
